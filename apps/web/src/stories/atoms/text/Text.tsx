a2cf105310d499b741656b8d9ce1036b783f3ed1
coderkartikeya <kartikeyavats04@gmail.com>
2026-03-21 19:48:07 +0530

    Storybook and tailwind added

---
+++ apps/web/src/stories/atoms/text/Text.tsx
@@ -0,0 +1,17 @@
+// import { type ReactNode } from "react";
+// import { twMerge } from "tailwind-merge";
+// import clsx from "clsx";
+
+// type fontSize = "sm" | "lg" | "xl" | "xs";
+// type fontWeight = "bold" | "regular" | "bold";
+// type ButtonProps = {
+//   fontSize?: fontSize;
+//   className: ReactNode;
+//   fontWeight?: fontWeight;
+//   fontColor?: string;
+// };
+const Text = () => {
+  return <p style={{}}></p>;
+};
+
+export default Text;
